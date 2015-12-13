13beefac5e37e6fa7f39509c6d0acb11ba4bb1f3
Anand Pandey <anand.etalk@gmail.com>
2015-12-13 23:58:17 +0530

    Login - Sign up, Sign In and Reset Password using email id

--- web/js/app.js
+++ web/js/app.js
@@ -1,8 +1,16 @@
 angular.module('app', ['ionic']);
 
+// models
+
+// ng-model holding values from view/html
+creds = {
+    name: "user",
+    password: "password",
+    email : "temp_email@temp.com"
+};
 
 function home() {
-    window.location = "home.html";
+    window.location = "index.html";
 }
 
 
@@ -28,4 +36,15 @@ function orderSummary(orderTId) {
 
 function congrats() {
     window.location = "congrats.html";
+}
+
+function signup() {
+    window.location = "signup.html";
+}
+function signin() {
+    window.location = "signin.html";
+}
+
+function forgotpassword() {
+    window.location = "forgot_password.html";
 }
